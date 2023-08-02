95f6d5fac4a68b765d7d1039deb3f974e037847d
Mustafa Eideh <mustafaeideh100@outlook.com>
2023-08-01 23:58:59 -0300

    Navbar components

--- garagefriesian/src/components/layout/Navbar.js
+++ garagefriesian/src/components/layout/Navbar.js
@@ -1,35 +1,49 @@
-import { Link } from 'react-router-dom';
-import { FaBars } from 'react-icons/fa'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+import { SidebarData } from "./SidebarData";
 
-import Container from './Container';
+import Container from "./Container";
 
-import styles from './Navbar.module.css';
-import logo from '../../img/GHorseF.png'
+import styles from "./Navbar.module.css";
+import logo from "../../img/GHorseF.png";
 
 function Navbar() {
-    return(
-        <nav>
-            <Container>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/educacao">Educação</Link>
-                    </li>
-                    <li>
-                        <Link to="/categoria">Categoria</Link>
-                    </li>
-                    <li>
-                        <Link to="/blog">Blog</Link>
-                    </li>
-                    <li>
-                        <Link to="/sobrenos">Sobre Nos</Link>
-                    </li>
-                </ul>
-            </Container>
-      </nav>
-    )
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      {/* <Container> */}
+        <div className={styles.navbar}>
+          <Link to="#" className={styles.menubars}>
+            <FaBars onClick={showSidebar} />
+          </Link>
+        </div>
+
+        <nav className={`${styles.navmenu} ${sidebar ? styles.active : ""}`}>
+          <ul className={styles.navmenuitems} onClick={showSidebar}>
+            <li className={styles.navbartoggle}>
+              <Link to="#" className={styles.menubars}>
+                <AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      {/* </Container> */}
+    </>
+  );
 }
 
 export default Navbar;
---
+++ garagefriesian/src/components/layout/SidebarData.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import styles from '../layout/Navbar.module.css'
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        cName: styles.navtext
+    },
+    {
+        title: 'Educação',
+        path: '/educacao',
+        cName: styles.navtext
+    },
+    {
+        title: 'Categorias',
+        path: '/categoria',
+        cName: styles.navtext
+    },
+    {
+        title: 'Blog',
+        path: '/blog',
+        cName: styles.navtext
+    },
+    {
+        title: 'Sobre nós',
+        path: '/sobrenos',
+        cName: styles.navtext
+    },
+]
