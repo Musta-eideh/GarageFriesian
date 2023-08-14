dcc945dce695fcad83ce2f157e181cce44cd3306
Mustafa Eideh <mustafaeideh100@outlook.com>
2023-08-14 17:07:38 -0300

    Page home, no text

--- garagefriesian/src/components/pages/Home.js
+++ garagefriesian/src/components/pages/Home.js
@@ -1,6 +1,40 @@
+import styles from './Home.module.css';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { register } from 'swiper/element/bundle'
+import 'swiper/css'
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+register();
+
 function Home() {
+
+    const slideFotos = [
+        {id: '1', image: 'https://img.olhardigital.com.br/wp-content/uploads/2022/04/tesla-roadster.webp'},
+        {id: '2', image: 'https://img.olhardigital.com.br/wp-content/uploads/2022/04/tesla-roadster.webp'},
+        {id: '3', image: 'https://img.olhardigital.com.br/wp-content/uploads/2022/04/tesla-roadster.webp'},
+        {id: '4', image: 'https://img.olhardigital.com.br/wp-content/uploads/2022/04/tesla-roadster.webp'},
+    ]
+
     return (
-        <h1>Home</h1>
+        <div className={styles.containerHome}>
+            <Swiper
+                slidesPerView={1}
+                pagination={{clickble: true}}
+                navigation
+            >
+                {slideFotos.map((item) => (
+                    <SwiperSlide key={item.id}>
+                        <img 
+                            src={item.image}
+                            alt="Slider"
+                            className={styles.slideItem}
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+        </div>
     )
 }
 
